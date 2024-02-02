12585ab7d3755167971c7ab1b2184831001f93a9
Rusty <proga100@gmail.com>
2024-02-03 01:59:52 +0500

    dd

--- background.js
+++ background.js
@@ -59,3 +59,104 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	}
 });
 
+let stream;
+let mediaRecorder;
+let recordedChunks = [];
+let recording = false;
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'startRecording' && !recording) {
+    startRecording();
+  } else if (request.action === 'stopRecording' && recording) {
+    stopRecording();
+  }
+});
+
+async function startRecording() {
+  try {
+    stream = await getDesktopStream();
+
+    mediaRecorder = new MediaRecorder(stream);
+
+    mediaRecorder.ondataavailable = function (event) {
+      if (event.data.size > 0) {
+        recordedChunks.push(event.data);
+      }
+    };
+
+    mediaRecorder.onstop = function () {
+      const blob = new Blob(recordedChunks, { type: 'video/webm' });
+      recordedChunks = [];
+      let blobUrl = URL.createObjectURL(blob);
+      chrome.storage.local.set({'recordedVideoBlob': blobUrl}, function () {
+        console.log('Video Blob saved to storage');
+      });
+    };
+
+    mediaRecorder.start();
+
+    // Update recording status
+    recording = true;
+  } catch (error) {
+    console.error('Error starting recording:', error);
+  }
+}
+
+async function getDesktopStream() {
+  return new Promise((resolve, reject) => {
+    chrome.desktopCapture.chooseDesktopMedia(['screen', 'window'], (streamId) => {
+      if (!streamId) {
+        reject(new Error('User cancelled desktop capture.'));
+        return;
+      }
+
+      navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          mandatory: {
+            chromeMediaSource: 'desktop',
+            chromeMediaSourceId: streamId
+          }
+        }
+      })
+      .then(resolve)
+      .catch(reject);
+    });
+  });
+}
+
+function stopRecording() {
+  if (mediaRecorder && mediaRecorder.state !== 'inactive') {
+    mediaRecorder.stop();
+    stream.getTracks().forEach(track => track.stop());
+
+    // Update recording status
+    recording = false;
+  }
+}
+
+	function downloadVideo() {
+		chrome.storage.local.get(['recordedVideoBlob'], function (result) {
+			const blobUrl = result.recordedVideoBlob;
+			console.log(blobUrl);
+			if (blobUrl) {
+				chrome.downloads.download({
+					url: blobUrl,
+					filename: 'recorded_video.webm',
+					saveAs: false
+				}, function (downloadId) {
+					if (chrome.runtime.lastError) {
+						console.error('Error downloading video:', chrome.runtime.lastError);
+					} else {
+						console.log('Video download started with ID:', downloadId);
+					}
+				});
+
+				// Clean up the Blob URL after the download is initiated
+				URL.revokeObjectURL(blobUrl);
+			} else {
+				console.error('Recorded video Blob not found in storage');
+			}
+		});
+	}
+
