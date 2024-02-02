46016dd107795c50213b707e192279f99caa5f18
Rusty <proga100@gmail.com>
2024-02-02 21:36:42 +0500

    Update content.js

--- content.js
+++ content.js
@@ -1,20 +1,22 @@
-console.log('ttt');
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === 'capture') {
-    chrome.tabs.captureVisibleTab(null, { format: 'png' }, function (dataUrl) {
-      localStorage.setItem('screenshot', dataUrl);
-    });
-  } else if (request.action === 'createVideo') {
-    const screenshots = JSON.parse(localStorage.getItem('screenshots')) || [];
-    const video = document.createElement('video');
+document.addEventListener('DOMContentLoaded', function () {
+	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+		if (request.action === 'capture') {
+			chrome.tabs.captureVisibleTab(null, {format: 'png'}, function (dataUrl) {
+				localStorage.setItem('screenshot', dataUrl);
+			});
+		} else if (request.action === 'createVideo') {
+			const screenshots = JSON.parse(localStorage.getItem('screenshots')) || [];
+			const video = document.createElement('video');
 
-    screenshots.forEach(function (screenshot) {
-      const img = document.createElement('img');
-      img.src = screenshot;
-      video.appendChild(img);
-    });
+			screenshots.forEach(function (screenshot) {
+				const img = document.createElement('img');
+				img.src = screenshot;
+				video.appendChild(img);
+			});
 
-    document.body.appendChild(video);
-    video.play();
-  }
+			document.body.appendChild(video);
+			video.play();
+		}
+	});
 });
+
