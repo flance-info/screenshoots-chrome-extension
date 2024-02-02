16bb844d34cd982ecb4d4cfb7fe3aa8a2ba42d08
Rusty <proga100@gmail.com>
2024-02-03 00:17:28 +0500

    Update background.js

--- background.js
+++ background.js
@@ -1,26 +1,36 @@
 console.log('Content script loaded');
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log('onMessage');
-  if (request.action === 'capture') {
-    console.log('capture');
+	console.log('onMessage');
+	if (request.action === 'capture') {
 
-    chrome.tabs.captureVisibleTab(null, { format: 'png' }, function (dataUrl) {
-      console.log(dataUrl);
-      localStorage.setItem('screenshot', dataUrl);
-    });
-  } else if (request.action === 'createVideo') {
-    const screenshots = JSON.parse(localStorage.getItem('screenshots')) || [];
-    const video = document.createElement('video');
 
-    screenshots.forEach(function (screenshot) {
-      const img = document.createElement('img');
-      img.src = screenshot;
-      video.appendChild(img);
-    });
+		// Use chrome.tabs.captureVisibleTab directly in the background script
+		chrome.tabs.captureVisibleTab(null, {format: 'png'}, function (dataUrl) {
 
-    document.body.appendChild(video);
-    video.play();
-  }
+			// Save the captured dataUrl to chrome.storage
+			chrome.storage.local.set({'screenshot': dataUrl}, function () {
+				console.log('Screenshot URL saved to storage');
+			});
+
+
+			chrome.storage.local.get(["screenshot"]).then((result) => {
+				console.log("Value is " + result.screenshot);
+			});
+		});
+
+	} else if (request.action === 'createVideo') {
+		const screenshots = JSON.parse(localStorage.getItem('screenshots')) || [];
+		const video = document.createElement('video');
+
+		screenshots.forEach(function (screenshot) {
+			const img = document.createElement('img');
+			img.src = screenshot;
+			video.appendChild(img);
+		});
+
+		document.body.appendChild(video);
+		video.play();
+	}
 });
 
